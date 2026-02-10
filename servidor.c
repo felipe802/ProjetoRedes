de1c704d38f186f7d71c5d7933b7112c081263c0
felipe802 <felipejleite18@gmail.com>
2026-02-10 17:10:41 -0300

    Atualização arquivo
    Criamos o socket e a estrutura envolta dele para o envio.

--- servidor.c
+++ servidor.c
@@ -1,8 +1,23 @@
-/* 
-socket() cria o endpoint de comunicacao
-bind() associa o socket a uma porta especifica
-listen() coloca o socket em modo de espera por conexoes
-accept() retira uma conexao da fila e cria um novo socket para essa conversa
-read()/write() troca de dados (onde entra o protocolo HTTP)
-close() encerra a conexao
-*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h> // Define a interface de sockets. Aqui estão as funções e a conexão feita na rede
+#include <netinet/in.h> // Define as estruturas de endereçamento da internet (IPv4/IPv6)
+
+#define PORTA 8080
+
+int main() {
+    // 1. Criar socket e definir a estrutura dele.
+    int serv_file_desc;
+    if ((serv_file_desc = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("Erro ao criar socket!\n");
+        return (EXIT_FAILURE);
+    }
+    struct sockaddr_in ender_serv;
+    memset(&ender_serv, 0, sizeof(ender_serv));
+    ender_serv.sin_family = AF_INET; // IPv4
+    ender_serv.sin_addr.s_addr = INADDR_ANY;
+    ender_serv.sin_port = htons(PORTA); // Inverter a sequência dos bytes
+    
+    close(serv_file_desc);
+    return 0;
+}
